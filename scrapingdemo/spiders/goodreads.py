c4965d81957e44d52ab2ef4cc875983e2d8b4044
ankitjaadoo <talk2ankit007@gmail.com>
2019-09-14 15:22:50 +0530

    commit 2

--- scrapingdemo/spiders/goodreads.py
+++ scrapingdemo/spiders/goodreads.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class GoodreadsSpider(scrapy.Spider):
     #identity
     name = 'goodreads'
     #requests
     allowed_domains = ['goodreads.com']
     def start_requests(self):
-        start_urls = [
-        'https://goodreads.com/quotes?page=1',
-        'https://goodreads.com/quotes?page=2',
-        'https://goodreads.com/quotes?page=3',
-        'https://goodreads.com/quotes?page=4',
-        'https://goodreads.com/quotes?page=5'
-    ]
-        for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+
+        url = 'https://goodreads.com/quotes?page=1'
+        yield scrapy.Request(url=url, callback=self.parse)
+
     #response
     def parse(self, response):
-        page_number = response.url.split("=")[1]
-        _file = f'{page_number}.html'
-        with open(_file,'wb') as f:
-            f.write(response.body)
+        for quote in response.selector.xpath("//div[@class='quote']"):
+            yield{
+                'text': quote.xpath(".//div[@class='quoteText']/text()[1]").extract_first(),
+                'author': quote.xpath(".//div[@class='quoteText']/child::span/text()").extract_first(),
+                'tags': quote.xpath(".//div[@class='greyText smallText left']/a/text()").extract(),    
+            }
+        next_page = response.selector.xpath("a[@class='next_page]/@href").extract_first()
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)        
+
+
+
 
 
         
